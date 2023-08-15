9f9feeec8a3b302743880cd321c5dee881852f28
PMNicolas39 <lengocson0309@gmail.com>
2023-08-15 21:55:47 +0700

    Second commit: Add the technique open from another file.txt

--- Day 20 - Snake game/scoreboard.py
+++ Day 20 - Snake game/scoreboard.py
@@ -9,6 +9,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
+        with open("new_text.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 200)
@@ -16,7 +19,7 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"score: {self.score} high score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -26,3 +29,11 @@ class Scoreboard(Turtle):
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("new_text.txt", mode="w") as file:
+                file.write(f"high score {self.high_score}")
+        self.score = 0
+        self.update_score()
